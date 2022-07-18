36c0cc92d5c379b941226eb8001b69b44236ad42
rjswn97 <81522323+bakgom123@users.noreply.github.com>
2022-07-18 14:10:41 +0900

    Add files via upload

---
+++ pepe/index.js
@@ -0,0 +1,6 @@
+setInterval(()=>{
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a,i)=>{
+    a.src = "https://i.kym-cdn.com/entries/icons/original/000/017/618/pepefroggie.jpg"
+    })
+}, 500)
